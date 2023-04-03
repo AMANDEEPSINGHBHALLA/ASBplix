fb2dd417c177356173a3caec67b48c44a8f2dcd7
AMANDEEPSINGHBHALLA <112118649+AMANDEEPSINGHBHALLA@users.noreply.github.com>
2023-04-03 20:51:14 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,50 @@
+const splash = document.querySelector(".splash");
+const dropdownbutton = document.querySelector(".dropdownbutton");
+const userdropdownbutton = document.querySelector(".userdropdownbutton");
+
+document.addEventListener("DOMContentLoaded", (e)=>{
+    setTimeout(()=>{
+        splash.classList.add("display-none");
+    }, 5000);
+})
+
+dropdownbutton.addEventListener("click", ()=>{
+    document.querySelector(".drop_down").classList.add("drop_down_active");
+})
+
+document.getElementsByClassName("cross")[0].addEventListener("click", ()=>{
+    document.querySelector(".drop_down").classList.remove("drop_down_active");
+})
+
+userdropdownbutton.addEventListener("click", ()=>{
+    document.querySelector(".user_button").classList.add("user_button_active");
+})
+document.getElementsByClassName("cross2")[0].addEventListener("click", ()=>{
+    document.querySelector(".user_button").classList.remove("user_button_active");
+})
+
+document.getElementsByClassName("dd-home")[0].addEventListener("click", ()=>{
+    document.location.href = "index.html";
+})
+document.getElementsByClassName("dd-plans")[0].addEventListener("click", ()=>{
+    window.location.href = "https://asbplix-plans.netlify.app/";
+})
+
+document.getElementsByClassName("try-for-free")[0].addEventListener("click", ()=>{
+    window.location.href = "https://asbplix-signup.netlify.app/";
+})
+
+document.getElementsByClassName("get-started")[0].addEventListener("click", ()=>{
+    window.location.href = "https://asbplix-signup.netlify.app/";
+})
+
+document.getElementsByClassName("playstore_icon")[0].addEventListener("click", ()=>{
+    window.location.href = "https://play.google.com/";
+})
+document.getElementsByClassName("apple_icon")[0].addEventListener("click", ()=>{
+    window.location.href = "https://www.apple.com/in/app-store/";
+})
+
+document.getElementsByClassName("user-button-tryforfree")[0].addEventListener("click", ()=>{
+    window.location.href = "https://asbplix-signup.netlify.app/";
+})
